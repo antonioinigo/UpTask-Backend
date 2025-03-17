e4e4b27630059f3461e98bb1d51ca7b9e322f526
Antonio Inigo <ainigocampos@gmail.com>
2025-03-17 16:55:16 +0000

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,18 @@
 import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
+    origin: function (origin, callback) {
+        console.log('Origin recibido:', origin); // Log para depurar
+        const whitelist = [process.env.FRONTEND_URL];
 
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
+        if (process.argv[2] === '--api') {
+            whitelist.push(undefined);
         }
 
-        if(whitelist.includes(origin)) {
-            callback(null, true)
+        if (!origin || whitelist.includes(origin)) {
+            callback(null, true); // Permitir el origen
         } else {
-            callback(new Error('Error de CORS'))
+            callback(new Error('Error de CORS'));
         }
     }
-}
+};
