b329c479edaf610ebae8d1b6e0604cd9d9442b27
Antonio Inigo <ainigocampos@gmail.com>
2025-03-17 18:29:57 +0000

    editando cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,19 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
+  origin: function (origin, callback) {
+    const whitelist = [process.env.FRONTEND_URL, 'https://up-task-frontend-nu-eight.vercel.app'];
 
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
-
-        if(whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+    // Permitir solicitudes sin origen (usado por herramientas como Postman)
+    if (process.argv[2] === '--api') {
+      whitelist.push(undefined);
     }
-}
+
+    // Verificar si la solicitud proviene de un origen permitido
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  },
+};
