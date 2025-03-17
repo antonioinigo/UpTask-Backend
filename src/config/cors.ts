449213f27adb33dc4f696dc589d9145256dfd730
Antonio Inigo <ainigocampos@gmail.com>
2025-03-17 18:38:33 +0000

    pasandpo de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,19 +1,17 @@
-import { CorsOptions } from 'cors';
+import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL, 'https://up-task-frontend-nu-eight.vercel.app'];
+    origin: function(origin, callback) {
+        const whitelist = [process.env.FRONTEND_URL]
 
-    // Permitir solicitudes sin origen (usado por herramientas como Postman)
-    if (process.argv[2] === '--api') {
-      whitelist.push(undefined);
-    }
+        if(process.argv[2] === '--api') {
+            whitelist.push(undefined)
+        }
 
-    // Verificar si la solicitud proviene de un origen permitido
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Error de CORS'));
+        if(whitelist.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
     }
-  },
-};
+}
